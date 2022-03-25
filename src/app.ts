34354344d136902fe8f3777cd8d995b5cc548a7d
wjx0820 <1989wjx@gmail.com>
2022-03-25 21:00:39 +0800

    server start

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log('111')
+import Fastify from "fastify"
+
+const server = Fastify()
+const port = 3000
+
+server.get("/healthcheck", async function () {
+  return { status: "ok" }
+})
+
+async function main() {
+  try {
+    await server.listen(port, "0.0.0.0", () => {
+      console.log("Server listening on " + port)
+    }) // '0.0.0.0' is for docker
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+}
+
+main()
